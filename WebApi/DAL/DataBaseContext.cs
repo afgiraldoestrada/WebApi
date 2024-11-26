1f5abc24bef08d3d02977f4588f9b37f5173bcea
Andres Felipe Giraldo Estrada <afgiraldoestrada@gmail.com>
2024-11-26 08:50:59 -0500

    Actualización String conexion a BD

--- WebApi/DAL/DataBaseContext.cs
+++ WebApi/DAL/DataBaseContext.cs
@@ -1,11 +1,29 @@
-﻿namespace WebApi.DAL
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.DAL.Entities;
+
+namespace WebApi.DAL
 {
-    //Instalar
+    //Instalar(importante)
     //Microsoft.EntityFrameworkCore V6.0.22
     //Microsoft.EntityFrameworkCore.Tools V6.0.22
     //Microsoft.EntityFrameworkCore.SqlServer V6.0.22
+    //Microsoft.EntityFrameworkCore.Design V6.0.22
     public class DataBaseContext : DbContext
     {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) //Asi me conecto a la base de datos por medio de este constructor
+        {
+            
+        }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        #region DbSets
+
+        public DbSet<Country> Countries { get; set; }
+
+        #endregion
     }
 }
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Línea de código que necesito para configurar la conexion a la base de datos.
+builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
