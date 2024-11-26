ffd0c345c0891e18b4b284e97b0d260c3e26da5d
Andres Felipe Giraldo Estrada <afgiraldoestrada@gmail.com>
2024-11-26 15:22:44 -0500

    Services layer developed

--- WebApi/Domain/Services/CountryService.cs
+++ WebApi/Domain/Services/CountryService.cs
@@ -1,6 +1,99 @@
-﻿namespace WebApi.Domain.Services
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.DAL;
+using WebApi.DAL.Entities;
+using WebApi.Domain.Interfaces;
+
+namespace WebApi.Domain.Services
 {
-    public class CountryService
+    public class CountryService : ICountryService
     {
+        private readonly DataBaseContext _context;
+
+        public CountryService(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Country>> GetCountriesAsync()
+        {            
+            try
+            {
+                var countries = await _context.Countries.ToListAsync();
+                return countries;
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
+            }
+
+        }
+
+        public async Task<Country> GetCountryByIdAsync(Guid id)
+        {            
+            try
+            {
+                var country = await _context.Countries.FirstOrDefaultAsync(c => c.Id == id);
+                return country;
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
+            }
+        }
+        public async Task<Country> CreateCountryAsync(Country country)
+        {            
+            try
+            {
+                country.Id = Guid.NewGuid();
+                country.CreatedDate = DateTime.Now;
+                _context.Countries.Add(country);//Permite crear el objeto en el contexto de mi BD.
+                await _context.SaveChangesAsync();//Permite guardar el pais en la tabla COUNTRY.
+                return country;    
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
+            }
+        }
+        public async Task<Country> EditCountryAsync(Country country)
+        {
+            try
+            {
+                country.ModifiedDate = DateTime.Now;
+                _context.Countries.Update(country); //Vitualizo el objeto
+                await _context.SaveChangesAsync();
+                return country;
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
+            }
+        }
+
+        public async Task<Country> DeleteCountryAsync(Guid id)
+        {
+            try
+            {
+                var country = await GetCountryByIdAsync(id);
+
+                if (country == null)
+                {
+                    return null;
+                }
+
+                _context.Countries.Remove(country);
+                await _context.SaveChangesAsync();
+                return country;
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
+            }
+        }
+
+        
+
+
+        
     }
 }
