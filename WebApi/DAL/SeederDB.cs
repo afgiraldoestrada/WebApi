aed578edf4d8d845daea63585f4f212f372f76ad
Andrés Giraldo <afgiraldoestrada@gmail.com>
2024-11-26 20:42:00 -0500

    Created SeederBD

---
+++ WebApi/DAL/SeederDB.cs
@@ -0,0 +1,69 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using WebApi.DAL.Entities;
+
+namespace WebApi.DAL
+{
+    public class SeederDB
+    {
+        private readonly DataBaseContext _context;
+        public SeederDB(DataBaseContext context) //Esto y lo anterior es pura inyección de dependencia
+        {
+            _context = context;
+        }
+
+        //Creamos un metodo llamado SeederAsync => Es una especio de metodo main() => Este metodo tendrá la responsabilidad de repoblar mis diferentes tablas de las BD
+        public async Task SeederAsync()
+        {
+            //Agregare un metodo propio de EntityFramework que hace las veces del comando update database
+            await _context.Database.EnsureCreatedAsync();
+
+            //A partir de aqui vamos a crear metodos que sirven para poblar la BD
+            await PopulateCountriesAsync();
+
+            await _context.SaveChangesAsync(); //Esta linea guarda lo datos en la BD
+        }
+
+        #region
+        private async Task PopulateCountriesAsync()
+        {
+            if (!_context.Countries.Any()) //Si al menos hay un registro en la tabla
+            {
+                //Asi creo yo un objeto pais con sus respectivos estados
+                _context.Countries.Add(new Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Colombia",
+                    States = new List<State>()
+                    {
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Antioquia"
+                        },
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Cundinamarca"
+                        }
+                    }
+                });
+
+                //Aqui creo otro nuevo país 
+                _context.Countries.Add(new Country
+                {
+                    CreatedDate = DateTime.Now,
+                    Name = "Argentina",
+                    States = new List<State>()
+                    {
+                        new State
+                        {
+                            CreatedDate = DateTime.Now,
+                            Name = "Buenos Aires"
+                        }                        
+                    }
+                });
+            }
+        }
+        #endregion
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 using WebApi.DAL;
 using WebApi.Domain.Interfaces;
 using WebApi.Domain.Services;
@@ -12,14 +13,31 @@ builder.Services.AddControllers();
 //Línea de código que necesito para configurar la conexion a la base de datos.
 builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 //Contenedor de dependencias
 builder.Services.AddScoped<ICountryService, CountryService>(); //Creando el servicio para poder ser utilizado en el controlador
+//builder.Services.AddScoped<IStateService, StateService>();
+builder.Services.AddTransient<SeederDB>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+SeederData();
+
+void SeederData()
+{
+    IServiceScopeFactory? scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopeFactory.CreateScope())
+    {
+        SeederDB? service = scope.ServiceProvider.GetService<SeederDB>();
+        service.SeederAsync().Wait();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
